25b8370c4b47cfc2f94db7403e2c3c2fc6c21324
DESKTOP-452RB1P\burak <burakcinar07@gmail.com>
2017-07-10 16:38:58 +0300

    Service implementasyonlarına devam edildi.

---
+++ Humb.Core/Interfaces/ServiceInterfaces/IBookInteractionService.cs
@@ -0,0 +1,17 @@
+﻿using Humb.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humb.Core.Interfaces.ServiceInterfaces
+{
+    interface IBookInteractionService
+    {
+        bool AddInteraction(Book book, string email, int interactionType);
+        IList<BookInteraction> GetInteractions(int bookID);
+        int GetInteractionCount(int bookID);
+        bool CanAddInteraction(int interactionType, int bookState);
+    }
+}
---
+++ Humb.Core/Interfaces/ServiceInterfaces/ILovedGenreService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Humb.Core.Interfaces.ServiceInterfaces
+{
+    interface ILovedGenreService
+    {
+        void AddGenreCodes(string email, int[] genreCodes);
+        int[] GetUserLovedGenreCodes(string email);
+    }
+}
