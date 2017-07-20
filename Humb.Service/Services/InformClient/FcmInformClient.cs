6c82ac7ead178bc92667bea44b84d5c624564328
DESKTOP-452RB1P\burak <burakcinar07@gmail.com>
2017-07-20 18:13:52 +0300

    Basic inform client interface and it is implementation inform client class created.

--- Humb.Service/Services/InformClient/FcmInformClient.cs
+++ Humb.Service/Services/InformClient/FcmInformClient.cs
@@ -1,91 +1,61 @@
-﻿using Humb.Core.Interfaces.ServiceInterfaces.MessageInterfaces;
+﻿using Humb.Core.Constants;
+using Humb.Core.Interfaces.ServiceInterfaces.InformClientInterfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
-using Humb.Core.Entities;
-using Humb.Core.Constants;
-using System.Net;
-using System.Web;
-using Pelusoft.EasyMapper;
-using Humb.Core.DTOs;
-using System.IO;
 using System.Web.Script.Serialization;
 
-namespace Humb.Service.Services.MessageServiceProviders
+namespace Humb.Service.Services.InformClient
 {
-    public class FcmMessageSender : IMessageSender
+    class FcmInformClient : IInformClient
     {
-        public void SendMessage(int messageId, User fromUser, User toUser, string messageText)
+        public void Inform(string senderFcmToken, string recieverFcmToken, object fcmData)
         {
             try
             {
-                string applicationId = ResponseConstant.APPLICATION_ID;
-                string senderId = fromUser.FcmToken;
-                string receiverId = toUser.FcmToken;
+                string applicationID = ResponseConstant.APPLICATION_ID;
+                string senderId = senderFcmToken;
+
+                string receiverId = recieverFcmToken;
+
+
                 WebRequest tRequest = WebRequest.Create("https://fcm.googleapis.com/fcm/send");
 
                 tRequest.Method = "post";
 
                 tRequest.ContentType = "application/json";
-                var data = new
-                {
-
-                    to = receiverId,
-
-                    data = new
-                    {
-                        fcmDataType = ResponseConstant.FCM_DATA_TYPE_SENT_MESSAGE,
-                        message = messageText,
-                        messageID = messageId,
-                        sender = EasyMapper.Map<UserDTO>(fromUser)
-                    }
-                };
+                var data = fcmData;
 
                 var serializer = new JavaScriptSerializer();
-
                 var json = serializer.Serialize(data);
-
                 Byte[] byteArray = Encoding.UTF8.GetBytes(json);
-
-                tRequest.Headers.Add(string.Format("Authorization: key={0}", applicationId));
-
+                tRequest.Headers.Add(string.Format("Authorization: key={0}", applicationID));
                 tRequest.Headers.Add(string.Format("Sender: id={0}", senderId));
-
                 tRequest.ContentLength = byteArray.Length;
-
 
                 using (Stream dataStream = tRequest.GetRequestStream())
                 {
-
                     dataStream.Write(byteArray, 0, byteArray.Length);
-
-
                     using (WebResponse tResponse = tRequest.GetResponse())
                     {
-
                         using (Stream dataStreamResponse = tResponse.GetResponseStream())
                         {
-
                             using (StreamReader tReader = new StreamReader(dataStreamResponse))
                             {
-                                //Examine response from fcm
                                 String sResponseFromServer = tReader.ReadToEnd();
-
                                 string str = sResponseFromServer;
-
                             }
                         }
                     }
                 }
             }
-
             catch (Exception ex)
             {
             }
         }
-
-       
     }
 }
--- Humb.Service/Services/MessageService.cs
+++ Humb.Service/Services/MessageService.cs
@@ -6,76 +6,75 @@ using System.Text;
 using System.Threading.Tasks;
 using Humb.Core.Entities;
 using Humb.Core.Interfaces.RepositoryInterfaces;
-using Humb.Core.Interfaces.ServiceInterfaces.MessageInterfaces;
 using Humb.Core.Constants;
 
 namespace Humb.Service.Services
 {
-    public class MessageService : IMessageService
+    public class MessageService //: IMessageService
     {
-        private readonly IRepository<Message> _messageRepository;
-        private readonly IMessageSender _messageSender;
-        private readonly IUserService _userService;
-        public MessageService(IRepository<Message> messageRepo, IMessageSender messageSender, IUserService userService)
-        {
-            _messageRepository = messageRepo;
-            _messageSender = messageSender;
-            _userService = userService;
-        }
-        public int CreateMessage(int fromUserId, int toUserId, string messageText)
-        {
-            Message message = new Message()
-            {
-                FromUserId = fromUserId,
-                ToUserId = toUserId,
-                MessageText = messageText,
-                FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_SENT,
-                ToUserMessageState = ResponseConstant.MESSAGE_TO_USER_STATE_NONE,
-                CreatedAt = DateTime.Now
-            };
-            _messageRepository.Insert(message);
-            _messageSender.SendMessage(message.Id, _userService.GetUser(fromUserId), _userService.GetUser(toUserId), messageText);
-            return message.Id;
-        }
+        //private readonly IRepository<Message> _messageRepository;
+        //private readonly IMessageSender _messageSender;
+        //private readonly IUserService _userService;
+        //public MessageService(IRepository<Message> messageRepo, IMessageSender messageSender, IUserService userService)
+        //{
+        //    _messageRepository = messageRepo;
+        //    _messageSender = messageSender;
+        //    _userService = userService;
+        //}
+        //public int CreateMessage(int fromUserId, int toUserId, string messageText)
+        //{
+        //    Message message = new Message()
+        //    {
+        //        FromUserId = fromUserId,
+        //        ToUserId = toUserId,
+        //        MessageText = messageText,
+        //        FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_SENT,
+        //        ToUserMessageState = ResponseConstant.MESSAGE_TO_USER_STATE_NONE,
+        //        CreatedAt = DateTime.Now
+        //    };
+        //    _messageRepository.Insert(message);
+        //    _messageSender.SendMessage(message.Id, _userService.GetUser(fromUserId), _userService.GetUser(toUserId), messageText);
+        //    return message.Id;
+        //}
 
-        public void DeleteConversation(string email, int toUserId)
-        {
-            throw new NotImplementedException();
-        }
+        //public void DeleteConversation(string email, int toUserId)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public void DeleteMessages(string email, int[] messageIds)
-        {
-            throw new NotImplementedException();
-        }
+        //public void DeleteMessages(string email, int[] messageIds)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public IEnumerable<Message> GetFetchedMessages(string email, int[] userIds)
-        {
-            throw new NotImplementedException();
-        }
+        //public IEnumerable<Message> GetFetchedMessages(string email, int[] userIds)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        public int GetFromUserIDByMessageID(int messageId)
-        {
-            return _messageRepository.FindSingleBy(x => x.Id == messageId).FromUserId;
-        }
+        //public int GetFromUserIDByMessageID(int messageId)
+        //{
+        //    return _messageRepository.FindSingleBy(x => x.Id == messageId).FromUserId;
+        //}
 
-        public void UpdateMessageState(int messageId, string email, int messageType)
-        {
-            Message m = _messageRepository.FindSingleBy(x => x.Id == messageId);
-            int userId = _userService.GetUserId(email);
-            if (!(m.FromUserId == userId) && !(m.ToUserId == userId))
-                return;
+        //public void UpdateMessageState(int messageId, string email, int messageType)
+        //{
+        //    Message m = _messageRepository.FindSingleBy(x => x.Id == messageId);
+        //    int userId = _userService.GetUserId(email);
+        //    if (!(m.FromUserId == userId) && !(m.ToUserId == userId))
+        //        return;
             
-            if (messageType == ResponseConstant.MESSAGE_TYPE_DELIVERED)
-            {
-                m.ToUserMessageState = ResponseConstant.MESSAGE_TO_USER_STATE_RECIEVED;
-                m.FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_DELIVERED;
-                _messageRepository.Save();
-            }
-            else if (messageType == ResponseConstant.MESSAGE_TYPE_SEEN)
-            {
-                m.FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_SEEN;
-                _messageRepository.Save();
-            }
-        }
+        //    if (messageType == ResponseConstant.MESSAGE_TYPE_DELIVERED)
+        //    {
+        //        m.ToUserMessageState = ResponseConstant.MESSAGE_TO_USER_STATE_RECIEVED;
+        //        m.FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_DELIVERED;
+        //        _messageRepository.Save();
+        //    }
+        //    else if (messageType == ResponseConstant.MESSAGE_TYPE_SEEN)
+        //    {
+        //        m.FromUserMessageState = ResponseConstant.MESSAGE_FROM_USER_STATE_SEEN;
+        //        _messageRepository.Save();
+        //    }
+        //}
     }
 }
